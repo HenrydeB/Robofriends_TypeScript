f197a9836eb0b1fe6875851e81a0af6b0a113605
Henry deBuchananne <henrycd95@hotmail.com>
2021-05-13 10:44:14 -0500

    first push

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
-const SearchBox = ({ searchChange }) => {
+interface isSearchBoxProps {
+  searchChange(event: React.SyntheticEvent<HTMLInputElement>): void;
+}
+
+// searchChange is an event that doesn't change the state. Events for react is pretty much what we typed above and is the industry standard of how we do this.
+// We do the HTMLInputElement because as we see below we have and Input element for HTML, and since we don't return anything, we put the type void
+
+const SearchBox = ({ searchChange } : isSearchBoxProps) => {
   return (
     <div className='pa2'>
       <input
